8ead40cd8c4b4776e0009bd9a6a6ae04bad8a344
LUCA DURANDO <s303395@studenti.polito.it>
2022-10-17 11:03:08 +0200

    Added DB

--- backend/DB.sql
+++ backend/DB.sql
@@ -12,25 +12,20 @@ CREATE TABLE `ticket` (
   `id` INTEGER,
   `service` INTEGER,
   `eta` INTEGER,
-  `served` INTEGER  0 or 1
+  `served` INTEGER,
+  FOREIGN KEY (`service`) REFERENCES `service` (`id`)
 );
 
 CREATE TABLE `ServiceCounter` (
   `service` INTEGER,
-  `counter` INTEGER
+  `counter` INTEGER,
+  FOREIGN KEY (`service`) REFERENCES `service` (`id`),
+  FOREIGN KEY (`counter`) REFERENCES `counter` (`id`)
 );
 
 CREATE TABLE `serving` (
   `ticket` INTEGER,
-  `counter` INTEGER
+  `counter` INTEGER,
+  FOREIGN KEY (`ticket`) REFERENCES `ticket` (`id`),
+  FOREIGN KEY (`counter`) REFERENCES `counter` (`id`)
 );
-
-ALTER TABLE `ticket` ADD FOREIGN KEY (`service`) REFERENCES `service` (`id`);
-
-ALTER TABLE `ServiceCounter` ADD FOREIGN KEY (`service`) REFERENCES `service` (`id`);
-
-ALTER TABLE `ServiceCounter` ADD FOREIGN KEY (`counter`) REFERENCES `counter` (`id`);
-
-ALTER TABLE `serving` ADD FOREIGN KEY (`ticket`) REFERENCES `ticket` (`id`);
-
-ALTER TABLE `serving` ADD FOREIGN KEY (`counter`) REFERENCES `counter` (`id`);
