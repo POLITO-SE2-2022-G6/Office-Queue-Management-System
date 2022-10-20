95af81192b9b60df8e266db89835e58eca881c3e
EGE UNLUSOY s305007 <s305007@studenti.polito.it>
2022-10-20 16:35:33 +0200

    frontend add ticket

---
+++ frontend/src/API.js
@@ -0,0 +1,33 @@
+import Service from "./Service";
+const SERVER_URL = 'http://localhost:3001';
+
+
+const addTicket = async (serviceId) => {
+    try {
+        const response = await fetch(SERVER_URL + '/api/ticket', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({service: serviceId}),
+        });
+
+        if (!response.ok) {
+            const errMessage = await response.json();
+            throw errMessage;
+        }
+        else return null;
+    } catch (err) {
+        throw new Error('Cannot communicate with the server.<br/>Error: ' + err);
+    }
+}
+
+const getServices = async () => {
+    const response = await fetch(SERVER_URL + '/api/service', {
+    });
+    const serviceJson = await response.json();
+    if(response.ok) {
+      return serviceJson.map(s => new Service(s.id,s.type));
+    }
+    else
+      throw serviceJson;
+  };
+export default {addTicket,getServices} ;
