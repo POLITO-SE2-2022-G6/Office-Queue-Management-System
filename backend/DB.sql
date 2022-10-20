51ba6156796056f783f821c719b3a58eb25889d7
LUCA DURANDO <s303395@studenti.polito.it>
2022-10-20 15:16:50 +0200

    Updated DB.sql

--- backend/DB.sql
+++ backend/DB.sql
@@ -1,31 +1,34 @@
-CREATE TABLE `service` (
-  `id` INTEGER,
-  `type` text,
-  `time` INTEGER
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "serving" (
+	"ticket"	INTEGER,
+	"counter"	INTEGER,
+	FOREIGN KEY("ticket") REFERENCES "ticket"("id"),
+	FOREIGN KEY("counter") REFERENCES "counter"("id"),
+	PRIMARY KEY("ticket","counter")
 );
-
-CREATE TABLE `counter` (
-  `id` INTEGER
+CREATE TABLE IF NOT EXISTS "service" (
+	"id"	INTEGER,
+	"type"	text,
+	"time"	INTEGER,
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
-
-CREATE TABLE `ticket` (
-  `id` INTEGER,
-  `service` INTEGER,
-  `eta` INTEGER,
-  `served` INTEGER,
-  FOREIGN KEY (`service`) REFERENCES `service` (`id`)
+CREATE TABLE IF NOT EXISTS "ticket" (
+	"id"	INTEGER,
+	"service"	INTEGER,
+	"eta"	INTEGER,
+	"served"	INTEGER DEFAULT 0,
+	FOREIGN KEY("service") REFERENCES "service"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
 );
-
-CREATE TABLE `ServiceCounter` (
-  `service` INTEGER,
-  `counter` INTEGER,
-  FOREIGN KEY (`service`) REFERENCES `service` (`id`),
-  FOREIGN KEY (`counter`) REFERENCES `counter` (`id`)
+CREATE TABLE IF NOT EXISTS "counter" (
+	"id"	INTEGER,
+	PRIMARY KEY("id")
 );
-
-CREATE TABLE `serving` (
-  `ticket` INTEGER,
-  `counter` INTEGER,
-  FOREIGN KEY (`ticket`) REFERENCES `ticket` (`id`),
-  FOREIGN KEY (`counter`) REFERENCES `counter` (`id`)
+CREATE TABLE IF NOT EXISTS "ServiceCounter" (
+	"service"	INTEGER,
+	"counter"	INTEGER,
+	FOREIGN KEY("counter") REFERENCES "counter"("id"),
+	FOREIGN KEY("service") REFERENCES "service"("id"),
+	PRIMARY KEY("service","counter")
 );
+COMMIT;
